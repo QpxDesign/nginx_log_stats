86d8c57c07997c064f2ba9c390a71b268b0237d2
Quinn555 <71609644+Quinn555@users.noreply.github.com>
2023-11-08 10:15:06 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import argparse
+import re
+from datetime import datetime
+
+parser = argparse.ArgumentParser(
+                    prog='nginx_log_stats',
+                    description='gives you a statsitcal view of NGINX requests from an access.log',
+                    epilog='For support, contact Quinn (https://github.com/qpxdesign)')
+
+
+parser.add_argument('-f', '--file', help='file to search in (your NGINX access.log)')
+parser.add_argument('-s', '--search', help='General search term to match specific log lines (like for User Agents, specific IPs, etc), either plaintext or regex')
+parser.add_argument('-b', '--start_date',help='must also provide -ed (end date), find logs within given timespan, provide like 08/Nov/2023:08:25:12')
+parser.add_argument('-e','--end_date', help='provide like 08/Nov/2023:08:25:12')
+parser.add_argument('-w', '--host')
+parser.add_argument('-r', '--request')
+parser.add_argument('-st', '--status')
+
+args = parser.parse_args()
+
+if args.file == None:
+    raise Exception("File must be provided (your access.log).")
+
+if (args.start_date == None and args.end_date != None) or (args.start_date != None and args.end_date == None):
+    raise Exception("You must provide both an end date (-ed) and a start-date (-sd)")
+
+def keep_log(line):
+    parsed_line = parse_line(line)
+    if args.search is not None and re.search(r''+str(args.search),string=line) is None:
+        return False
+    if args.start_date is not None and (parse_nginx_time_format(parsed_line['time']) > parse_nginx_time_format(args.end_date) or parse_nginx_time_format(parsed_line['time']) < parse_nginx_time_format(args.start_date)):
+        return False
+    if args.host is not None and parsed_line["host"] != args.host:
+        return False
+    if args.request is not None and args.request not in parsed_line["request"]:
+        return False
+    if args.status is not None and parsed_line["status"] != args.status:
+        return False
+    return True
+
+def parse_line(line):
+    fields = line.split(" ")
+    return {
+            "ip_address":fields[0],
+            "time":fields[3].replace("[",""),
+            "host":fields[5],
+            "request":f'{fields[6]} {fields[7]}',
+            "status":fields[9]
+            }
+
+def parse_nginx_time_format(time):
+    return datetime.strptime(time,"%d/%b/%Y:%H:%M:%S")
+
+if __name__ == "__main__":
+    with open(f'./{args.file}', 'r') as f:
+        final_lines = []
+        lines = f.readlines()
+        for line in lines:
+            if keep_log(line):
+                final_lines.append(line)
+        for line in final_lines:
+            print(line)
