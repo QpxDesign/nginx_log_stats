7e066671d420475eb4c63152cf06836e05be04c3
qpxdesign <qpxdesign@users.noreply.github.com>
2023-11-11 17:50:38 -0500

    added unique ips to analytics view, bug fixes

--- nginx_log_stats.py
+++ nginx_log_stats.py
@@ -77,6 +77,7 @@ def main():
                 "request_count":0,
                 "top_5_requests":{},
                 "top_5_hosts":{},
+                "top_5_ips":{},
                 "average_body_byte_speed":0,
                 "average_requests_per_minute":0,
                 }
@@ -90,15 +91,23 @@ def main():
             if parsed_line["request"] not in stats["top_5_requests"]:
                 stats["top_5_requests"][parsed_line["request"]] = {
                         "request_text":parsed_line["request"],
-                        "count": 1
+                        "count": 0
                         }
             stats["top_5_requests"][parsed_line["request"]]["count"] += 1
 
             if parsed_line["host"] not in stats["top_5_hosts"]:
                 stats["top_5_hosts"][parsed_line["host"]] = {
                         "host_text":parsed_line["host"],
-                        "count": 1
+                        "count": 0
                         }
+
+            if parsed_line["ip_address"] not in stats["top_5_ips"]:
+                stats["top_5_ips"][parsed_line["ip_address"]] = {
+                        "ip_text":parsed_line["ip_address"],
+                        "count":0
+                        }
+
+            stats["top_5_ips"][parsed_line["ip_address"]]["count"] += 1
             stats["top_5_hosts"][parsed_line["host"]]["count"] += 1
 
         stats["average_body_byte_speed"] = stats["average_body_byte_speed"] / stats["request_count"]
@@ -106,25 +115,36 @@ def main():
 
         new_requests = []
         new_hosts = []
+        new_ips = []
         for request,entry in stats["top_5_requests"].items():
             new_requests.append(entry)
 
         for host,entry in stats["top_5_hosts"].items():
             new_hosts.append(entry)
 
+        for ip,entry in stats["top_5_ips"].items():
+            new_ips.append(entry)
+
         new_hosts.sort(key=lambda x:x != None and x.get("count"),reverse=True)
         new_requests.sort(key=lambda x:x != None and x.get("count"),reverse=True)
+        new_ips.sort(key=lambda x:x != None and x.get("count"),reverse=True)
 
         stats["top_5_requests"] = new_requests[:5]
         stats["top_5_hosts"] = new_hosts[:5]
+        stats["top_5_ips"] = new_ips[:5]
 
         top_5_hosts_output = ""
         top_5_requests_output = ""
+        top_5_ips_output = ""
         for item in stats["top_5_hosts"]:
-            top_5_hosts_output += f"-{item['host_text']} ~ {format(item['count'],',d')} \n".replace('"','')
+            top_5_hosts_output += f"- {item['host_text']} ~ {format(item['count'],',d')} \n".replace('"','')
 
         for item in stats["top_5_requests"]:
-            top_5_requests_output += f"-{item['request_text']} ~ {format(item['count'],',d')} \n".replace('"','')
+            top_5_requests_output += f"- {item['request_text']} ~ {format(item['count'],',d')} \n".replace('"','')
+
+        for item in stats["top_5_ips"]:
+            if item["ip_text"] is not "-":
+                top_5_ips_output += f"- {item['ip_text']} ~ {format(item['count'],',d')} \n".replace('"','')
 
         print(f"""
 ===~ LOG SELECTION STATS ~===
@@ -136,6 +156,8 @@ Top 5 Requests:
 {top_5_requests_output}
 Top 5 Hosts:
 {top_5_hosts_output}
+Top 5 IP Addresses:
+{top_5_ips_output}
 """)
     with open(f'./{args.file}', 'r') as f:
         final_lines = []
